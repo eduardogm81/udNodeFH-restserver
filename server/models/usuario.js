9f5e90a362db48b4386af84f05f172fa924cec38
eduardogm81qg <QuarkGluon123>
2018-04-11 13:29:31 +0200

    Implementación de delete borrando al usuario de la base de datos

--- server/models/usuario.js
+++ server/models/usuario.js
@@ -45,7 +45,6 @@ usuarioSchema.methods.toJSON = function () {
     let user = this;
     let userObject = user.toObject();
     delete userObject.password;
-    console.log(userObject);
     return userObject;
 };
 
