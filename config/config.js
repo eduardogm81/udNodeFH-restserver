c442ee285ee3454d54b28badb8f945856b970210
eduardogm81qg <QuarkGluon123>
2018-04-11 15:17:13 +0200

    Añadida la base de datos de producción

--- config/config.js
+++ config/config.js
@@ -2,3 +2,17 @@
 // Puerto
 // =============================
 process.env.PORT = process.env.PORT || 3000;
+// =============================
+// Entorno
+// =============================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+// =============================
+// Base de datos
+// =============================
+let urlDb;
+if (process.env.NODE_ENV === 'dev') {
+    urlDb = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDb = `mongodb://cafe-user:123456@ds241489.mlab.com:41489/cafe`;
+}
+process.env.URLDB = urlDb;
